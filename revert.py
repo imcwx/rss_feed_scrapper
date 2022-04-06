f844a90c91eee0973f50a5df8100fef31d4263f5
imcwx <chewwenxiang@gmail.com>
2022-04-06 13:33:40 +0800

    helper scripts and function

--- revert.py
+++ revert.py
@@ -10,7 +10,6 @@ type(feed_tracker['breaking_news']['last_modified'])
 new_last_modified = 'Tue, 05 Apr 2022 14:00:00 GMT'
 
 for feed_name, trackers in feed_tracker.items():
-    # print(feed_name, trackers)
     last_modified = trackers.get('last_modified')
     print(feed_name, last_modified)
     trackers['last_modified'] = new_last_modified
@@ -18,3 +17,10 @@ for feed_name, trackers in feed_tracker.items():
 
 with open(TRACKER_PATH + '_new_2', 'wb') as handle:
     pickle.dump(feed_tracker, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+# import feedparser
+# rss_feed = "https://www.straitstimes.com/news/asia/rss.xml"
+# news_feed = feedparser.parse(rss_feed)
+#
+# for i in news_feed.entries:
+#     print(i)
