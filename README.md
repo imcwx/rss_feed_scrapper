573f3c894740dd4adce1b1339f8562f8cd4411c2
imcwx <chewwenxiang@gmail.com>
2022-04-09 00:16:52 +0800

    Added CNA feeed. yet to do full run test

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ By tracking RSS Feed, we are able to scrap data from some sites.
 This is a test of concept and only implemented to Straits Time RSS Feed.
 
 Since it is successful, it should be applicable to expand to various sites with RSS Feed.
+
+- Added CNA feed but have not done full run.
+
+
+Other things to add:
+- Parallel runs for various feeds or just leave it for different 'bots' or 'servers' to run individually.
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import sys
 import time
 import os
 import pickle
 import logging
 from datetime import datetime
+import argparse
 import feedparser
 import pandas as pd
 import urllib.request
@@ -16,14 +18,29 @@ def create_logger(logger_file_name):
     date_time_now = datetime.now().strftime('%Y%m%d_%H%M%S')
     log_file_name = "{}_{}.log".format(logger_file_name, date_time_now)
     # create file handler which logs even debug messages
-    fh = logging.FileHandler(log_file_name)
-    fh.setLevel(logging.INFO)
+    file_handler = logging.FileHandler(log_file_name)
+    file_handler.setLevel(logging.INFO)
     # create formatter and add it to the handlers
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(funcName)s - %(levelname)s - %(message)s')
-    fh.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+    # create stdout handle
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setLevel(logging.INFO)
+    stdout_handler.setFormatter(formatter)
     # add the handlers to the logger
-    logger.addHandler(fh)
+    logger.addHandler(file_handler)
+    logger.addHandler(stdout_handler)
     return logger
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='RSS Feed - Data scraper')
+    parser.add_argument('--feed',
+                        default='st',
+                        choices=['st', 'cna'],
+                        nargs='?',
+                        help='Choose feed to download')
+    return parser.parse_args()
 
 
 def get_feed(rss_feed, modified_time=None, etag=None):
@@ -76,13 +93,16 @@ def init_feed(rss_feed_links):
     return data, feed_tracker
 
 
-def get_feed_update(feed_tracker, last_modified=True, last_etag=False):
+def get_feed_update(feed_tracker, feed_update='last_modified', last_etag=False):
     new_data = {}
     for feed_name, trackers in feed_tracker.items():
-        if last_modified:   # Get feed update using modified time
+        if feed_update == 'last_modified':   # Get feed update using modified time
             new_feed = get_feed(trackers.get('rss_feed_link'), modified_time=trackers.get('last_modified'))
-        else:   # Else use etag
+        elif feed_update == 'last_etag':
             new_feed = get_feed(trackers.get('rss_feed_link'), etag=trackers.get('etag'))
+        else:
+            logger.error("Feed update is not last_modified or last_etag, please implement or choose a method.")
+            exit()
         new_data[feed_name] = new_feed
     return new_data
 
@@ -99,16 +119,16 @@ def update_tracker(rss_feed, feed_tracker, feed_name):
     }
 
 
-def save_data(data, feed_tracker, init=False):
+def save_data(data, feed_tracker, data_path, init=False):
     # Save the data entries
     if init:
         # Initialize data path if not exist
-        if not os.path.exists(DATA_PATH):
-            os.mkdir(DATA_PATH)
+        if not os.path.exists(data_path):
+            os.makedirs(data_path, exist_ok=True)
 
     # Loop through each feed
     for feed_name, rss_feed in data.items():
-        data_feed_path = os.path.join(DATA_PATH, feed_name)
+        data_feed_path = os.path.join(data_path, feed_name)
         record_path = os.path.join(data_feed_path, 'records.csv')
 
         if init:
@@ -140,12 +160,12 @@ def save_data(data, feed_tracker, init=False):
             # Request/pull actually article
             for entry in new_entries:
                 url = entry.get('link')
-                request = urllib.request.Request(url)
+                request = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/6.0'})
                 basename = os.path.basename(url)
                 file_path = os.path.join(data_feed_path, f"{basename}.html")
                 logger.info(f"Entry basename: {basename}")
                 try:
-                    response = urllib.request.urlopen(request)
+                    response = urllib.request.urlopen(request,)
                     data_entry = response.readlines()
                     data_string = [i.decode('utf-8') for i in data_entry]
                     with open(file_path, "w") as f:
@@ -153,6 +173,7 @@ def save_data(data, feed_tracker, init=False):
                 except Exception as e:
                     logger.exception("Error, cannot retrieve data")
                     logger.error(f"url : {url}")
+                time.sleep(PARTIAL_SLEEP)
         else:
             # No new entries
             logger.info("%s has 0 new entries" % feed_name)
@@ -165,16 +186,16 @@ def main():
             feed_tracker = pickle.load(handle)
     else:
         logger.info("No tracker found, initializing feed")
-        data, feed_tracker = init_feed(straits_time_rss_feed)
+        data, feed_tracker = init_feed(RSS_FEED)
         with open(TRACKER_PATH, 'wb') as handle:
             pickle.dump(feed_tracker, handle, protocol=pickle.HIGHEST_PROTOCOL)
-        save_data(data, feed_tracker, init=True)
+        save_data(data, feed_tracker, DATA_PATH, init=True)
 
     # Loop and update each feed.
     while True:
         logger.info("Get Feed Update")
-        new_data = get_feed_update(feed_tracker)
-        save_data(new_data, feed_tracker, init=False)
+        new_data = get_feed_update(feed_tracker, FEED_UPDATE)
+        save_data(new_data, feed_tracker, DATA_PATH, init=False)
         logger.info(f'--------wait {SLEEP_DURATION} seconds before checking again-------')
         time.sleep(SLEEP_DURATION)
 
@@ -183,23 +204,45 @@ if __name__ == '__main__':
     logger = create_logger('feed')
     logger.info("Program Started")
 
-    # Initiate parameters
-    TRACKER_PATH = 'feed_tracker.pickle'
-    DATA_PATH = 'data'
-    SLEEP_DURATION = 60
+    # Parse Args
+    args = parse_args()
+    feed = args.feed
+    logger.info(f"Feed : {feed}")
 
-    logger.info("straits_time_rss_feed does not support etags, use modified time")
-    straits_time_rss_feed = {
-        "breaking_news": "https://www.straitstimes.com/rss_breaking_news.opml",
-        "world": "https://www.straitstimes.com/news/world/rss.xml",
-        "business": "https://www.straitstimes.com/news/business/rss.xml",
-        "sport": "https://www.straitstimes.com/news/sport/rss.xml",
-        "life": "https://www.straitstimes.com/news/life/rss.xml",
-        "opinion": "https://www.straitstimes.com/news/opinion/rss.xml",
-        "singapore": "https://www.straitstimes.com/news/singapore/rss.xml",
-        "asia": "https://www.straitstimes.com/news/asia/rss.xml",
-        "tech": "https://www.straitstimes.com/news/tech/rss.xml",
-        "multimedia": "https://www.straitstimes.com/news/multimedia/rss.xml",
-    }
+    if feed == 'st':
+        # Initiate parameters
+        TRACKER_PATH = 'st_feed_tracker.pickle'
+        DATA_PATH = 'data/st'
+        SLEEP_DURATION = 60
+        PARTIAL_SLEEP = 0
+        FEED_UPDATE = 'last_modified'   # or 'etag'
+        logger.info("straits_time_rss_feed does not support etags, use modified time")
+        RSS_FEED = {
+            "breaking_news": "https://www.straitstimes.com/rss_breaking_news.opml",
+            "world": "https://www.straitstimes.com/news/world/rss.xml",
+            "business": "https://www.straitstimes.com/news/business/rss.xml",
+            "sport": "https://www.straitstimes.com/news/sport/rss.xml",
+            "life": "https://www.straitstimes.com/news/life/rss.xml",
+            "opinion": "https://www.straitstimes.com/news/opinion/rss.xml",
+            "singapore": "https://www.straitstimes.com/news/singapore/rss.xml",
+            "asia": "https://www.straitstimes.com/news/asia/rss.xml",
+            "tech": "https://www.straitstimes.com/news/tech/rss.xml",
+            "multimedia": "https://www.straitstimes.com/news/multimedia/rss.xml",
+        }
+    elif feed == 'cna':
+        # Initiate parameters
+        TRACKER_PATH = 'cna_feed_tracker.pickle'
+        DATA_PATH = 'data/cna'
+        SLEEP_DURATION = 60
+        PARTIAL_SLEEP = 0
+        FEED_UPDATE = 'last_modified'   # last_etag     last_modified
+        RSS_FEED = {
+            "latest_news": "https://www.channelnewsasia.com/api/v1/rss-outbound-feed?_format=xml",
+            "asia": "https://www.channelnewsasia.com/api/v1/rss-outbound-feed?_format=xml&category=6511",
+            "business": "https://www.channelnewsasia.com/api/v1/rss-outbound-feed?_format=xml&category=6936",
+            "singapore": "https://www.channelnewsasia.com/api/v1/rss-outbound-feed?_format=xml&category=10416",
+            "sport": "https://www.channelnewsasia.com/api/v1/rss-outbound-feed?_format=xml&category=10296",
+            "world": "https://www.channelnewsasia.com/api/v1/rss-outbound-feed?_format=xml&category=6311",
+        }
     main()
     logger.info("Program Completed")
